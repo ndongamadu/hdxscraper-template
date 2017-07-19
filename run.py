d9539916da122c226c0801b3fea604ad876d9f29
Michael Rans <rans@email.com>
2017-07-19 17:34:12 +0200

    Scraper template

--- run.py
+++ run.py
@@ -32,4 +32,7 @@ def main():
             showcase.add_dataset(dataset)
 
 if __name__ == '__main__':
-    facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'))
+    facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'),
+           smtp_config_yaml=join('config', 'smtp_configuration.yml'))  # Remove smtp_config_yaml if you don't want
+                                                                       # emails when there are errors
+
