27d60de6ac8fcb9f4f20a05d505d16f9c4537e5d
Amadu <ndongamadou@gmail.com>
2017-08-21 12:14:20 +0000

    fix

--- run.py
+++ run.py
@@ -6,43 +6,44 @@ Top level script. Calls other functions that generate datasets that this script 
 """
 import logging
 from os.path import join, expanduser
-
+import csv
 from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
 
-from scrapername import generate_dataset_and_showcase, get_countriesdata
+from wfp import generate_dataset_and_showcase
 
 # Remove 2 lines below if you don't want emails when there are errors
 from hdx.facades import logging_kwargs
-logging_kwargs['smtp_config_yaml'] = join('config', 'smtp_configuration.yml')
+#logging_kwargs['smtp_config_yaml'] = join('config', 'smtp_configuration.yml')
 
 from hdx.facades.hdx_scraperwiki import facade
+#from hdx.Configuration import Configuration
 
 logger = logging.getLogger(__name__)
 
 
 def main():
     """Generate dataset and create it in HDX"""
-
-    base_url = Configuration.read()['base_url']
-    # If website being scraped requires username and password, you can supply one in a file in your home directory.
-    # The file should contain username:password based64 encoded. Remember to create it on the ScraperWiki box!
-    # If you need to add extra parameters to every url, you can use extra_params_yaml and point to a YAML file with
-    # key value pairs. Remember to create it on the ScraperWiki box!
-    downloader = Download(basic_auth_file=join(expanduser("~"), '.scrapernamefile'),
-                          extra_params_yaml=join(expanduser("~"), 'scrapernamefile.yml')
-    countriesdata = get_countriesdata(base_url, downloader)
-    logger.info('Number of datasets to upload: %d' % len(countriesdata))
-    for countrydata in countriesdata:
-        dataset, showcase = generate_dataset_and_showcase(base_url, downloader, countrydata)
-        if dataset:
+    with open('countries.csv', 'r') as countriesList:
+        reader = csv.reader(countriesList)
+        for row in reader :
+            pays = row[0]
+            iso = row[1]
+            dataset, showcase = generate_dataset_and_showcase(pays, iso)
             dataset.update_from_yaml()
+            dataset.add_country_location(iso)
+
+            dataset.add_tags([iso,'OPENSTREETMAP','ROADS', 'UNSDIT'])
+            showcase.add_tags([iso,'OPENSTREETMAP','ROADS', 'UNSDIT'])
+
             dataset.create_in_hdx()
             showcase.create_in_hdx()
             showcase.add_dataset(dataset)
 
+            print('==== %s dataset added ====' %pays)
+
 if __name__ == '__main__':
     # Remember to create .hdxkey on the ScraperWiki box!
     # It is best to use the HDX Data Team bot's key (https://data.humdata.org/user/luiscape) rather than your own.
-    facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'))
-
+    #facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'))
+    facade(main, hdx_site='test')
---
+++ wfp.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+SCRAPERNAME:
+------------
+
+Reads ScraperName JSON and creates datasets.
+
+"""
+
+import logging
+
+from hdx.data.dataset import Dataset
+from hdx.data.resource import Resource
+from hdx.data.hdxobject import HDXError
+from hdx.data.showcase import Showcase
+from hdx.utilities.location import Location
+from slugify import slugify
+import time
+
+
+logger = logging.getLogger(__name__)
+
+def generate_dataset_and_showcase(country, countryISO3):
+    """Parse json of the form:
+    {
+    },
+    """
+    title = country.capitalize() + ' Road Network (main roads)'
+    #logger.info('Creating dataset: %s' % title)
+    slugified_name = slugify(title).lower()
+
+    dataset = Dataset({
+    })
+    dataset['name'] = slugified_name
+    dataset['title'] = title
+
+    datex = time.strftime("%x")
+    dataset.set_dataset_date(datex)
+    dataset.set_expected_update_frequency('Every day')
+    mainRoadsResource = Resource()
+    mainRoadsResource['name'] = title
+    mainRoadsResource['format'] = 'zipped shapefile'
+    mainRoadsResource['url_type'] = 'api'
+    mainRoadsResource['url'] = 'http://ogcserver.gis.wfp.org/geoserver/ows?format_options=charset:UTF-8&typename=geonode:'+countryISO3+'_trs_roads_osm&outputFormat=SHAPE-ZIP&version=1.0.0&service=WFS&request=GetFeature&hdx=hdx'
+    mainRoadsResource['description'] = 'Datasets contains : primary, secondary and tertiary roads'
+
+    dataset.add_update_resource(mainRoadsResource)
+
+    showcase = Showcase({
+        'name': '%s-showcase' % slugified_name,
+        'title': country.capitalize(),
+        'notes': 'WFP Geonode',
+        'url': 'https://geonode.wfp.org/layers/ogcserver.gis.wfp.org%3Ageonode%3A'+countryISO3+'_trs_roads_osm',
+        'image_url': ' '
+    })
+    #showcase.add_tags([])
+    print('==== dataset generated ====')
+    return dataset, showcase
